c5cb7d71bab12dedd64d8a44ddb76de4ad2205fb
Arjun K V <110488156+thearjunkv@users.noreply.github.com>
2025-02-09 16:37:48 +0530

    Added tests for GridLine component

--- tests/components/GridLine.test.tsx
+++ tests/components/GridLine.test.tsx
@@ -1,16 +1,57 @@
 import { render, screen } from '@testing-library/react';
 import GridLine from '../../src/components/GridLine';
+import userEvent from '@testing-library/user-event';
 
 describe('GridLine component', () => {
-	it('should be in the document', () => {
-		render(
+	let selectedBy: number | undefined;
+	const handleLineClick = vi.fn((value: number) => (selectedBy = value));
+
+	it('should be in the document with proper alignment', () => {
+		const { rerender } = render(
 			<GridLine
 				alignment='horizontal'
-				handleLineClick={() => {}}
-				selectedBy={undefined}
+				handleLineClick={() => handleLineClick(1)}
+				selectedBy={selectedBy}
 			/>
 		);
 
-		expect(screen.getByTestId('grid-line')).toBeInTheDocument();
+		const gridLine = screen.getByTestId('grid-line');
+		expect(gridLine).toHaveClass('grid-line--horizontal');
+
+		rerender(
+			<GridLine
+				alignment='vertical'
+				handleLineClick={() => handleLineClick(1)}
+				selectedBy={selectedBy}
+			/>
+		);
+		expect(gridLine).toHaveClass('grid-line--vertical');
+	});
+
+	it('should have class name of player-2 and grid-line--selected when clicked', async () => {
+		const { rerender } = render(
+			<GridLine
+				alignment='vertical'
+				handleLineClick={() => handleLineClick(2)}
+				selectedBy={selectedBy}
+			/>
+		);
+		const gridLine = screen.getByTestId('grid-line');
+
+		expect(gridLine).not.toHaveClass('grid-line--selected');
+		expect(gridLine).not.toHaveClass('player-2');
+
+		const user = userEvent.setup();
+		await user.click(gridLine);
+
+		rerender(
+			<GridLine
+				alignment='vertical'
+				handleLineClick={() => handleLineClick(2)}
+				selectedBy={selectedBy}
+			/>
+		);
+		expect(gridLine).toHaveClass('grid-line--selected');
+		expect(gridLine).toHaveClass('player-2');
 	});
 });
